ea66c73ed6f4eb01891429a5144e7bd7aaa9fa6f
LittleStupid <lttlstpd@live.com>
2017-03-23 21:13:16 +0800

    2.60

---
+++ 2.60.scm
@@ -0,0 +1,28 @@
+#lang sicp
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (cons x set))
+
+(define (union-set set1 set2)
+  (if (not (null? set1))
+      (union-set (cdr set1) (adjoin-set (car set1) set2))
+      set2))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+
+(define b (list 11 12 13 11))
+(define c (list 19 20 11))
+
+(intersection-set b c)
+(union-set b c)
